ccf6b5957133eed15f3c94520d0add8e215b0c16
Preston Wallace <preston.wallace@liquidityservices.com>
2019-06-26 08:27:01 -0700

    add router to heroku deploy script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const app = express();
+app.use(history())
 app.use(serveStatic(__dirname + "/dist"));
 const port = process.env.PORT || 5000;
 app.listen(port);
